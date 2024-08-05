4600516f90dfd0a608bfe510d4b96a78b518fb24
Jos-debug <jospinanda22@gmail.com>
2024-08-05 12:49:17 +0700

    Create header.php

---
+++ config/header.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
+    <title>Persaudaraan Pencak Slat Lebah Sakti</title>
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
+
+    <!-- Favicons -->
+  
+    <link href="assets/img/favicon.png" rel="icon" />
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500&family=Inter:wght@400;500&family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Vendor CSS Files -->
+    <link
+      href="assets/vendor/bootstrap/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="assets/vendor/bootstrap-icons/bootstrap-icons.css"
+      rel="stylesheet"
+    />
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+    <link
+      href="assets/vendor/glightbox/css/glightbox.min.css"
+      rel="stylesheet"
+    />
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
+
+    <!-- Template Main CSS Files -->
+    <link href="assets/css/variables.css" rel="stylesheet" />
+    <link href="assets/css/main.css" rel="stylesheet" />
+
+    <!-- =======================================================
+  * Template Name: ZenBlog
+  * Template URL: https://bootstrapmade.com/zenblog-bootstrap-blog-template/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https:///bootstrapmade.com/license/
+  ======================================================== -->
+  </head>
+
+  <body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="header d-flex align-items-center fixed-top">
+      <div
+        class="container-fluid container-xl d-flex align-items-center justify-content-between"
+      >
+        <a href="index.html" class="logo d-flex align-items-center">
+          <!-- Uncomment the line below if you also wish to use an image logo -->
+          <!-- <img src="assets/img/logo.png" alt=""> -->
+          <img src="assets/img/logoLs2.png" />
+          <h1>Persaudaraan Pencak Silat Lebah Sakti</h1>
+        </a>
+
+        <nav id="navbar" class="navbar">
+          <ul>
+            <li><a href="index.php">Beranda</a></li>
+            <li><a href="single-post.php">Berita</a></li>
+            <li class="dropdown">
+              <a href="#"
+                ><span>Organisasi</span>
+                <i class="bi bi-chevron-down dropdown-indicator"></i
+              ></a>
+              <ul>
+                <li><a href="about.php#struktur">Struktur Organisasi</a></li>
+                <li><a href="about.php#visimisi">Visi & Misi</a></li>
+                <li class="dropdown">
+                  <a href="#"
+                    ><span>Galeri</span>
+                    <i class="bi bi-chevron-down dropdown-indicator"></i
+                  ></a>
+                  <ul>
+                    <li><a href="#">Foto</a></li>
+                    <li><a href="#">Video</a></li>
+                    
+                  </ul>
+                </li>
+                <li><a href="#">Sejarah</a></li>
+                <li><a href="#">Drop Down 3</a></li>
+                <li><a href="#">Drop Down 4</a></li>
+              </ul>
+            </li>
+
+            <li><a href="about.php">Tentang Kami</a></li>
+            <li><a href="contact.php">Kontak</a></li>
+          </ul>
+        </nav>
+        <!-- .navbar -->
+
+        <div class="position-relative">
+          <a href="https://www.facebook.com/share/YqkjDiqDjqtp731T/?mibextid=LQQJ4d" class="mx-2"><span class="bi-facebook"></span></a>
+          <a href="https://www.tiktok.com/@pps.lebah.sakti?_t=8oWkRksgHBN&_r=1" class="mx-2"><span class="bi-tiktok"></span></a>
+          <a href="https://www.instagram.com/pencaksilat_lebahsakti?igsh=b2g5Zm03bDN2dm41&utm_source=qr" class="mx-2"><span class="bi-instagram"></span></a>
+          <a href="https://youtube.com/@pencaksilatlebahsakti1954?si=lKV8y1aB3hgQ9wS_" class="mx-2"><span class="bi-youtube"></span></a>
+
+
+          <a href="#" class="mx-2 js-search-open"
+            ><span class="bi-search"></span
+          ></a>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+
+          <!-- ======= Search Form ======= -->
+          <div class="search-form-wrap js-search-form-wrap">
+            <form action="search-result.php" class="search-form">
+              <span class="icon bi-search"></span>
+              <input type="text" placeholder="Search" class="form-control" />
+              <button class="btn js-search-close">
+                <span class="bi-x"></span>
+              </button>
+            </form>
+          </div>
+          <!-- End Search Form -->
+        </div>
+      </div>
+    </header>
