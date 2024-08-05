74a375f19a9783d656c53096d57723873ea22824
Jos-debug <jospinanda22@gmail.com>
2024-08-05 11:45:25 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,196 @@
+<?php include('config/header.php'); ?>
+
+  <main id="main">
+    <section>
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="col-lg-12 text-center mb-5">
+            <h1 class="page-title">Profil Lebah Sakti</h1>
+          </div>
+        </div>
+
+        <div class="row mb-5" id="sejarah">
+
+          <div class="d-md-flex post-entry-2 half">
+            <a href="#" class="me-4 thumbnail">
+              <img src="assets/img/jospi.png" alt="" class="img-fluid">
+            </a>
+            <div class="ps-md-5 mt-4 mt-md-0">
+              <div class="post-meta mt-4">Tentang </div>
+              <h2 class="mb-4 display-4">Sekapur Sirih</h2>
+
+              <p>Pencak Silat merupakan salah satu budaya asli bangsa Indonesia, dimana banyak pakar dan pendekar pencak silat meyakini bahwa masyarakat Melayu menciptakan dan menggunakan ilmu beladiri ini sejak masa prasejarah untuk menghadapi alam yang keras agar dapat survive (bertahan hidup). </p>
+             <p>Di Provinsi Sumatera Selatan memiliki banyak Seni pencak silat yang tersebar di Kabupaten/Kota dan Desa. Salah satunya adalah KUNTAU. Kuntau ini berasal dari Desa Kayu Ara Kecamatan Sekayu Kabupaten Musi Banyuasin, diperkuat dengan SK yang dikeluarkan Pasirah Marga Mantri Melayu Kewedenaan Musi Ilir tertanggal 15 Maret 1952.  Mulai diajarkan sejak tahun 1954-1974 dilingkungan keluarga dan masyarakat desa Kayu Ara Sekayu. Pada tahun 1982 kuntau ini berkembang di Palembang. Kuntau ini diciptakan oleh Husin Ibal  hasil gabungan yang didapatkannya dari beberapa guru Kuntau diantaranya : 1.Zainuri, 2.Cemong, 3.Asla, 4.Ate, 5.Muin, 6.Tjik Usin, 7. Sastro. Pencak silat ini menganut aliran “Kuntau Melayu alias Kuntau Usin dari Marga Mantri Melayu”. Gerakan silat ini terdiri 12 tari dan diperkaya juga dengan Seni Olah Pernafasan bagi murid yang dianggap telah siap fisik maupun bathin. </p>
+             <p>Perguruan Pencak Silat Lebah Sakti bergabung di Ipsi kota Palembang pada tahun 1987 yang kemudian perlahan mulai melebarkan sayapnya masuk dan berkembang di Bangka Belitung pada tahun 2007 kemudian resmi menjadi anggota Ipsi Bangka pada tahun 2008, hingga pada tahun 2020 Perguruan Pencak Silat Lebah Sakti resmi menjadi anggota Ipsi provinsi Bangka Belitung, lalu mulai berkemang di daerah lainnya seperti Pengda provinsi Kepri, Pengda provinsi Jawa Barat, selanjutnya akan terus berkembang di daerah lainnya. Insyaallah siap mencetak generasi yang berkualitas dan beradab.</p>
+             <p>Salam Aksi Balas Budi</p>
+             <p>“Salam persaudaraan satu lebah satu keluarga sejuta saudara”.</p>
+            </div>
+          </div>
+
+          <div class="d-md-flex post-entry-2 half mt-5" id="visimisi">
+            <a href="#" class="me-4 thumbnail order-2">
+              <img src="assets/img/profil1.png" alt="" class="img-fluid">
+            </a>
+            <div class="pe-md-5 mt-4 mt-md-0">
+              <div class="post-meta mt-4">Visi &amp; Misi</div>
+              <h2 class="mb-4 display-4">Visi</h2>
+
+              <p>Persaudaraan Pencak Silat Lebah Sakti bertujuan mempersatukan, membina Persaudaraan, Kebersamaan dan Kesetiakawanan  antar Organisasi atau keluarga/anggota  dalam rangka meningkatkan peran serta  persaudaraaan pencak silat lebah sakti dalam membangun manusia indonesia seutuhnya, serta mengakat harkat dan martabat bangsa.</p>
+              <h2 class="mb-4 display-4">Misi</h2>
+              <h6 class="mb-8 display-8"><b>1. Komunikasi &amp; Koordinasi</b> </h6>
+              <p>a.	Menggabungkan diri kedalam wadah seni bela diri Ikatan Pencak Silat Indonesia ( IPSI )</p>
+              <p>b.	Mengadakan kerjasama dengan organisasi lain yang tidak bertentangan dengan program Pemerintah dalam usaha pengembangan organisasi.</p>
+              <p>c.	Melaksanakan koordinasi dan komunikasi serta meningkatkan kerjasama antara anggota  organisasi.</p><br>
+              <h6 class="mb-8 display-8"><b>2. Partisipasi</b></h6>
+              <p>a.	Mengusahakan organisasi ini mendapat tempat di masyarakat dan pemerintah sebagai wadah untuk lebih mengembangkan lagi seni beladiri Pencak Silat agar dapat mengikuti perkembangan zaman.</p>
+              <p>b.	Menyelenggarakan usaha-usaha yang bersifat menunjang Pemerintah dalam usaha turut menjaga ketertiban dan keamanan serta ketahanan Nasional dimasyarakat.</p><br>
+              <h6 class="mb-8 display-8"><b>3. Kesejahteraan Anggota</b></h6>
+              <p>Menyelenggarakan usaha-usaha yang bertujuan untuk meningkatkan kesejahteraan anggota, baik kesejahteraan fisik, materiil maupun kesejahteraan yang tidak bertentangan dengan azaz dan tujuan organis</p>
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+
+    <section id="struktur" class="contact mb-5">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-12 text-center mb-5">
+            <h1 class="page-title">Stuktur Organisasi</h1>
+          </div>
+            <h3 class="mb-8 display-8"><b>1. Pengurus Pusat</b> </h3>
+         <p>Berikut Adalah Susunan Pengurus Pusat Persaudaraan Pencak Silat Lebah Sakti 2024-2030:</p>
+        <table class="table table-striped">
+        <tr>
+            <td colspan="2">Sekertaris</td>
+            <td>: Katrina Sahnaz</td>
+        </tr>
+        <tr>
+            <td colspan="2">Dapartemen Pembinaan Organisasi</td>
+            <td>: 1.Somad</td>
+        </tr>
+        <tr>
+            <td colspan="2"></td>
+            <td>: 2.Raffi</td>
+        </tr>
+        <tr>
+            <td colspan="2">Dapartemen Pembinaan Prestasi Fighter & Seni</td>
+            <td>: Raffi</td>
+        </tr>
+        <tr>
+            <td colspan="2">Dapartemen Pembinaan Seni Budaya</td>
+            <td>: Hermansayah, S.E</td>
+        </tr>
+        <tr>
+            <td colspan="2">Dapartemen Penelitian Dan Pengembangan</td>
+            <td>: Thawfik Hidayat</td>
+        </tr>
+        <tr>
+            <td colspan="2">Dapartemen Pembinaan Kerohanian</td>
+            <td>: Andika</td>
+        </tr>
+        <tr>
+            <td colspan="2">Dapartemen Promosi, Redaksi, Admin, Dan Editor</td>
+            <td>: Windy Trisna Fela</td>
+        </tr>
+        <tr>
+            <td colspan="1" rowspan="2">Dapartemen Promosi</td>
+            <td>1. Redaksi, Redaktur Pelaksana</td>
+            <td>:Desi</td>
+        </tr>
+        <tr>
+            <td>2. Dokumentalis</td>
+            <td>: desi</td>
+        </tr><tr>
+            <td colspan="2">Dapartemen Pembibitan Dan Pemasalan</td>
+            <td>: Suwardian</td>
+        </tr>
+        
+        </table>
+
+        </div>
+    </div>
+    </section>
+
+    <section class="mb-5 bg-light py-5">
+      <div class="container" data-aos="fade-up">
+        <div class="row justify-content-between align-items-lg-center">
+          <div class="col-lg-5 mb-4 mb-lg-0">
+            <h2 class="display-4 mb-4">Latest News</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, rem eaque vel est asperiores iste pariatur placeat molestias, rerum provident ea maiores debitis eum earum esse quas architecto! Minima, voluptatum! Minus tempora distinctio quo sint est blanditiis voluptate eos. Commodi dolore nesciunt culpa adipisci nemo expedita suscipit autem dolorum rerum?</p>
+            <p>At magni dolore ullam odio sapiente ipsam, numquam eius minus animi inventore alias quam fugit corrupti error iste laboriosam dolorum culpa doloremque eligendi repellat iusto vel impedit odit cum. Sequi atque molestias nesciunt rem eum pariatur quibusdam deleniti saepe eius maiores porro quam, praesentium ipsa deserunt laboriosam adipisci. Optio, animi!</p>
+            <p><a href="#" class="more">View All Blog Posts</a></p>
+          </div>
+          <div class="col-lg-6">
+            <div class="row">
+              <div class="col-6">
+                <img src="assets/img/post-portrait-3.jpg" alt="" class="img-fluid mb-4">
+              </div>
+              <div class="col-6 mt-4">
+                <img src="assets/img/post-portrait-4.jpg" alt="" class="img-fluid mb-4">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container" data-aos="fade-up" id="tokoh">
+        <div class="row">
+          <div class="col-12 text-center mb-5">
+            <div class="row justify-content-center">
+              <div class="col-lg-6">
+                <h2 class="display-4">Tokoh</h2>
+                <p>LEBAH TIDAK AKAN MENGGANGGU MANUSIA, KECUALI MANUSIA MENGGANGGUNYA. MADUNYA BERMANFAAT BAGI MANUSIA.</p>
+                <p>“Sopan Santun dan Berbudaya, Satu Lebah Satu Keluarga Sejuta Saudara”</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 text-center mb-5" >
+            <img src="assets/img/person-1.jpg" alt="" class="img-fluid rounded-circle w-50 mb-4">
+            <h4>Cameron Williamson</h4>
+            <span class="d-block mb-3 text-uppercase">Founder &amp; CEO</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis sunt ullam temporibus aut!</p>
+          </div>
+          <div class="col-lg-4 text-center mb-5">
+            <img src="assets/img/person-2.jpg" alt="" class="img-fluid rounded-circle w-50 mb-4">
+            <h4>Wade Warren</h4>
+            <span class="d-block mb-3 text-uppercase">Founder, VP</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis sunt ullam temporibus aut!</p>
+          </div>
+          <div class="col-lg-4 text-center mb-5">
+            <img src="assets/img/person-3.jpg" alt="" class="img-fluid rounded-circle w-50 mb-4">
+            <h4>Jane Cooper</h4>
+            <span class="d-block mb-3 text-uppercase">Editor Staff</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis sunt ullam temporibus aut!</p>
+          </div>
+          <div class="col-lg-4 text-center mb-5">
+            <img src="assets/img/person-4.jpg" alt="" class="img-fluid rounded-circle w-50 mb-4">
+            <h4>Cameron Williamson</h4>
+            <span class="d-block mb-3 text-uppercase">Editor Staff</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis sunt ullam temporibus aut!</p>
+          </div>
+          <div class="col-lg-4 text-center mb-5">
+            <img src="assets/img/person-5.jpg" alt="" class="img-fluid rounded-circle w-50 mb-4">
+            <h4>Cameron Williamson</h4>
+            <span class="d-block mb-3 text-uppercase">Editor Staff</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis sunt ullam temporibus aut!</p>
+          </div>
+          <div class="col-lg-4 text-center mb-5">
+            <img src="assets/img/person-6.jpg" alt="" class="img-fluid rounded-circle w-50 mb-4">
+            <h4>Cameron Williamson</h4>
+            <span class="d-block mb-3 text-uppercase">Editor Staff</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis sunt ullam temporibus aut!</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <?php include('config/footer.php'); ?>
---
+++ spengurus.php
@@ -0,0 +1,66 @@
+<?php include('config/header.php'); ?>
+<main id="main">
+    <section id="contact" class="contact mb-5">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-12 text-center mb-5">
+            <h1 class="page-title">Stuktur Pengurus</h1>
+          </div>
+            <h3 class="mb-8 display-8"><b>1. Pengurus Pusat</b> </h3>
+         <p>Berikut Adalah Susunan Pengurus Pusat Persaudaraan Pencak Silat Lebah Sakti 2024-2030:</p>
+        <table class="table table-striped">
+        <tr>
+            <td colspan="2">Sekertaris</td>
+            <td>: Katrina Sahnaz</td>
+        </tr>
+        <tr>
+            <td colspan="2">Dapartemen Pembinaan Organisasi</td>
+            <td>: 1.Somad</td>
+        </tr>
+        <tr>
+            <td colspan="2"></td>
+            <td>: 2.Raffi</td>
+        </tr>
+        <tr>
+            <td colspan="2">Dapartemen Pembinaan Prestasi Fighter & Seni</td>
+            <td>: Raffi</td>
+        </tr>
+        <tr>
+            <td colspan="2">Dapartemen Pembinaan Seni Budaya</td>
+            <td>: Hermansayah, S.E</td>
+        </tr>
+        <tr>
+            <td colspan="2">Dapartemen Penelitian Dan Pengembangan</td>
+            <td>: Thawfik Hidayat</td>
+        </tr>
+        <tr>
+            <td colspan="2">Dapartemen Pembinaan Kerohanian</td>
+            <td>: Andika</td>
+        </tr>
+        <tr>
+            <td colspan="2">Dapartemen Promosi, Redaksi, Admin, Dan Editor</td>
+            <td>: Windy Trisna Fela</td>
+        </tr>
+        <tr>
+            <td colspan="1" rowspan="2">Dapartemen Promosi</td>
+            <td>1. Redaksi, Redaktur Pelaksana</td>
+            <td>:Desi</td>
+        </tr>
+        <tr>
+            <td>2. Dokumentalis</td>
+            <td>: desi</td>
+        </tr><tr>
+            <td colspan="2">Dapartemen Pembibitan Dan Pemasalan</td>
+            <td>: Suwardian</td>
+        </tr>
+        
+        </table>
+
+        </div>
+    </div>
+    </section>
+</main>
+
+
+<?php include('config/footer.php'); ?>
